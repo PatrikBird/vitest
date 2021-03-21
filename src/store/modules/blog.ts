56828f50d253fe6ff62913fd40f239bc6ff49cdb
pvogel <patrik.vogel@paessler.com>
2021-03-21 11:41:32 +0100

    added blog types

--- src/store/modules/blog.ts
+++ src/store/modules/blog.ts
@@ -1,15 +1,24 @@
 // TODO: get json with blog posts as initial state from external file
+import { BlogPosts } from '../../types';
 
 // initial state
 const state = {
-  posts: [],
-};
+  posts: [
+    {
+      slug: 'my-post',
+      tag: 'vegan',
+      title: 'some-title',
+      subTitle: 'some-sub-title',
+      img: 'images/beach.jpg',
+      date: '2021-03-21',
+    },
+  ],
+} as BlogPosts;
 
 // getters
 const getters = {};
 
 export default {
-  //namespaced: true,
   state,
   getters,
 };
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
 import { ViteSSGContext } from 'vite-ssg';
 
 export type UserModule = (ctx: ViteSSGContext) => void;
+export type BlogPost = {
+  slug: string;
+  tag: string;
+  title: string;
+  subTitle: string;
+  img: string;
+  date: string;
+};
+export interface BlogPosts {
+  posts: BlogPost[];
+}
