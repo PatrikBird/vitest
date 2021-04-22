6712fba185d322eaa32d49fc40a72e72e58d9d3e
Patrik Bird <52401692+PatrikBird@users.noreply.github.com>
2021-04-22 20:11:16 +0200

    wip: filter by tag buttons and functionality

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
 import { blogPosts } from '../../blogPosts';
+
+function filterByTag(tag: String) {
+  return blogPosts.filter(ele => ele.tag === tag);
+}
 </script>
 
 <template>
   <div class="text-center">
     <carbon-pen-fountain class="m-auto" />
-    <h3>Blog</h3>
+    <h3 class="title font-xl">Blog</h3>
+
+    <div class="card bg-white py-3 px-5 rounded-xl flex flex-col mb-5">
+      <div class="title font-medium">Filter by Category</div>
+      <div class="w-full py-3">
+        <div class="flex inline-flex mr-2" role="group">
+          <button
+            @click="filterByTag('veganism')"
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 rounded-l-md hover:bg-blue-600 hover:shadow-lg"
+          >
+            Veganism
+          </button>
+          <button
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
+          >
+            Environmentalism
+          </button>
+          <button
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 rounded-r-md hover:bg-blue-600 hover:shadow-lg"
+          >
+            Future
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
+
   <div>
     <div class="max-w-5xl mx-auto pt-10 pb-10">
       <ul class="flex flex-wrap -mx-2 overflow-hidden list-none">
