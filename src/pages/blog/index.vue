6047ba7a26128d6c98a8efd15d5a8a77a2e61366
Patrik Bird <52401692+PatrikBird@users.noreply.github.com>
2021-04-30 14:39:29 +0200

    feat(common): add filter by category

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { blogPosts } from '../../blogPosts';
+import { ref, reactive, computed } from 'vue';
+import { blogPosts } from '~/blogPosts';
 
-function filterByTag(tag: String) {
-  return blogPosts.filter(ele => ele.tag === tag);
+let posts = reactive(blogPosts);
+let filterBy = ref('');
+let filteredPosts = computed(() => {
+  if (filterBy.value === '') return posts;
+  else return blogPosts.filter((ele) => ele.tag === filterBy.value);
+});
+
+function filterByTag(tag: string) {
+  filterBy.value = tag;
 }
 </script>
 
@@ -10,14 +18,50 @@ function filterByTag(tag: String) {
   <div class="text-center">
     <carbon-pen-fountain class="m-auto" />
     <h3 class="title font-xl">Blog</h3>
+
+    <div class="card bg-white py-3 px-5 rounded-xl flex flex-col mb-5">
+      <div class="title font-medium">Filter by Category</div>
+      <div class="w-full py-3">
+        <div class="flex inline-flex mr-2" role="group">
+          <button
+            @click="filterByTag('')"
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 rounded-l-md hover:bg-blue-600 hover:shadow-lg"
+          >
+            Reset
+          </button>
+          <button
+            @click="filterByTag('Veganism')"
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 rounded-l-md hover:bg-blue-600 hover:shadow-lg"
+          >
+            Veganism
+          </button>
+          <button
+            @click="filterByTag('Environmentalism')"
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
+          >
+            Environmentalism
+          </button>
+          <button
+            @click="filterByTag('Future')"
+            type="button"
+            class="focus:outline-none text-white text-sm py-2.5 px-5 bg-blue-500 rounded-r-md hover:bg-blue-600 hover:shadow-lg"
+          >
+            Future
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 
   <div>
     <div class="max-w-5xl mx-auto pt-10 pb-10">
       <ul class="flex flex-wrap -mx-2 overflow-hidden list-none">
         <blog-element
-          v-for="(ele, index) in blogPosts"
-          :key="ele.slug + '_' + index"
+          v-for="(ele, index) in filteredPosts"
+          :key="index"
           :slug="ele.slug"
           :tag="ele.tag"
           :title="ele.title"
